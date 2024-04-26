108831d2bc86eea61860d1e34ae75811ca53c95c
Hà Nguyễn Thị Thu <102210032@sv1.dut.udn.vn>
2024-04-26 11:22:50 +0700

    test api

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+import RPi.GPIO as GPIO
+import time
+GPIO. setmode(GPIO.BCM)
+GPIO.setup(25, GPIO.OUT)
+
+key = '5af21e4aab63472a8ff41909242604'
+location = 'Da Nang'
+ApiUrl = f"http://api.weatherapi.com/v1/current.json?key={key}&q={location}"
+while True:
+    r = requests.get(ApiUrl)
+    forecast = r.json()
+    popValue = forecast['current']['temp_c']
+    print(r.json())
+    # popValue = int(popValue)
+    if popValue >= 30:
+        GPIO.output(25, GPIO.HIGH)
+    else:
+        GPIO.output(25, GPIO.LOW)
+    time. sleep(180)
